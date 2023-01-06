70963e9273bd24daf86262402ca483d0b69bb90c
thangbuiq <21280048@student.hcmus.edu.vn>
2023-01-06 13:59:55 +0700

    Add Constants.py

---
+++ Constants.py
@@ -0,0 +1,6 @@
+import openai
+
+openai.api_key = 'HIDDEN'
+# token
+_PAGE_ACCESS_TOKEN = 'HIDDEN'
+_VERIFY_TOKEN = 'HIDDEN'
--- app.py
+++ app.py
@@ -1,62 +1,55 @@
 import json
 import aiohttp
 import openai
-import time
+import Constants as const
 from os import environ
 from aiohttp import web
-# Set the API - OpenAI
-openai.api_key = "HIDDEN"
-# token
-_PAGE_ACCESS_TOKEN = 'HIDDEN'
-_VERIFY_TOKEN = 'HIDDEN'
+from Constants import *
+
 # Choose the ChatGPT model
 _model_engine = "text-davinci-002" #large ver or davinci-002
 """
 text-davinci-002: A medium-sized version of ChatGPT with a capacity of around 1.5 billion parameters.
 text-curie-001: A small version of ChatGPT with a capacity of around 500 million parameters.
 text-babbage-001: A large version of ChatGPT with a capacity of around 3 billion parameters.
-"""
-def openaiGPT_request(question:str):
-    response = openai.Completion.create(engine=_model_engine, prompt=question + ". trả lời ngắn gọn.", max_tokens=1024, n=1,stop=None,temperature=0.5)
-    answer:str = "Câu trả lời chính thức của mình: \n" + response.choices[0].text
+""" 
+def openaiGPT_request(question:str, short:bool):
+    if (short == True):
+        question = question + ". Give me short answer."
+    response = openai.Completion.create(engine=_model_engine, prompt=question, max_tokens=1024, n=1,stop=None,temperature=0.5)
+    answer:str = "My final answer: \n" + response.choices[0].text
     return answer
 
 class BotControl(web.View):
     async def get(self):
         query = self.request.rel_url.query
         if(query.get('hub.mode') == "subscribe" and query.get("hub.challenge")):
-            if not query.get("hub.verify_token") == _VERIFY_TOKEN:
+            if not query.get("hub.verify_token") == const._VERIFY_TOKEN:
                 return web.Response(text='Verification token mismatch', status=403)
             return web.Response(text=query.get("hub.challenge"))
         return web.Response(text='Forbidden', status=403)
     async def post(self):
         data = await self.request.json()
         if data.get("object") == "page":
-            await self.send_greeting("Helu pé 🐄 Mình là bot trim non ụm pòa tạo bởi pa Théng nhí")
+            await self.send_greeting("Hello, I'm a Facebook-Chatbot using OpenAI-chatGPT to answer your question.")
             for entry in data.get("entry"):
                 for messaging_event in entry.get("messaging"):
                     if messaging_event.get("message"):
                         sender_id = messaging_event["sender"]["id"]
                         message_text = messaging_event["message"]["text"]
-                        #max_money:int = 200000
-                        #current_money:int = 0
-                        if any(["chào" in message_text.lower(), "hello" in message_text.lower(), "nhô" in message_text.lower()]):
-                            await self.send_message(sender_id, "chào pé iu cũa tớ 🐄")
-                        elif any(["tên" in message_text.lower(),"name" in message_text.lower()]):
-                            await self.send_message(sender_id, "mình tên là bot trimnon kute xicule xữa hột gè nha")
-                        elif any(["eh" in message_text.lower(), "lô" in message_text.lower()]):
-                            await self.send_message(sender_id, "kiu j đóa pò iu cũa pé")
+                        if any(["hello" in message_text.lower(), "hi" in message_text.lower()]):
+                            await self.send_message(sender_id, "Hello, how can I help you? Please ask me anything!")
                         else:
-                            await self.send_message(sender_id, "cái nì trim hok bíc bạn đợi trim chút để trim nghiên cứu nhé!")
-                            await self.send_message(sender_id, "đừng vội nhắn tin nhắn mới hãy đợi chút nhé")
+                            await self.send_message(sender_id, "Let me research this!")
+                            await self.send_message(sender_id, "You shouldn't send a new message while I'm researching.")
                             await self.send_message(sender_id,"...")
-                            await self.send_message(sender_id, openaiGPT_request(message_text))
-                            await self.send_message(sender_id, "hy vọng bạn hài lòng với câu trả lời trên 🐮")
+                            await self.send_message(sender_id, openaiGPT_request(message_text, True))
+                            await self.send_message(sender_id, "Hope you are satisfied with my answer.")
         return web.Response(text='ok', status=200)
 
     async def send_greeting(self, message_text):
         params = {
-            "access_token": _PAGE_ACCESS_TOKEN
+            "access_token": const._PAGE_ACCESS_TOKEN
         }
         headers = {
             "Content-Type": "application/json"
@@ -73,7 +66,7 @@ class BotControl(web.View):
     async def send_message(self, sender_id, message_text):
 
         params = {
-            "access_token": _PAGE_ACCESS_TOKEN
+            "access_token": const._PAGE_ACCESS_TOKEN
         }
         headers = {
             "Content-Type": "application/json"
@@ -99,6 +92,5 @@ routes = [
 
 app = web.Application()
 app.add_routes(routes)
-#hi
 #if __name__ == '__main__':
 #   web.run_app(app, host='0.0.0.0', port=environ.get("PORT", 9090))
